7793d0dd62cca93ceab3f19145c66523dd5b5de6
Bernard Luz <bernard@simpbank.com.br>
2026-01-11 16:10:07 -0300

    correção do theme-color

--- app.vue
+++ app.vue
@@ -3,7 +3,11 @@ useHead({
   link: [
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Great+Vibes&family=Montserrat:wght@300;400;500;600&display=swap' }
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Great+Vibes&family=Montserrat:wght@300;400;500;600&display=swap' },
+    { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+  ],
+  meta: [
+    { name: 'theme-color', content: '#EBF5F9' }
   ]
 })
 
@@ -14,8 +18,8 @@ useSeoMeta({
   ogDescription: 'Você é nosso convidado para celebrar os meus 50 anos!',
   ogImage: '/images/foto_hercinio.jpeg',
   twitterCard: 'summary_large_image',
-  themeColor: '#EBF5F9',
 })
+
 </script>
 
 
